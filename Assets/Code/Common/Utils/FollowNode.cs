510923516e71e5c9dab3a66c2c8651e7a9420744
farhinarius <farhat3027@gmail.com>
2023-11-04 02:24:56 +0300

    3d camera (work in progress)

--- Assets/Code/Common/Utils/FollowNode.cs
+++ Assets/Code/Common/Utils/FollowNode.cs
@@ -1,18 +1,19 @@
 using Godot;
 
+namespace dbasetemplate.Assets.Code.Common.Utils;
+
 public partial class FollowNode : Node
 {
     [Export]
-    private Node3D _nodeToFollow;
-    [Export]
     private Node3D _nodeFollower;
     [Export]
+    private Node3D _nodeToFollow;
+    [Export]
     private Vector3 _offset;
-
-    private Node _someNode;
 
     public override void _Process(double delta)
     {
         _nodeFollower.Position = _nodeToFollow.Position + _offset;
     }
+
 }
--- Assets/Code/Prototyping/Control/CubeController.cs
+++ Assets/Code/Prototyping/Control/CubeController.cs
@@ -1,18 +1,18 @@
 using Code.Prototyping.InputTranslation;
 using Godot;
 
-namespace ExploreGodot.Code.Control;
+namespace dbasetemplate.Assets.Code.Prototyping.Control;
 
 public partial class CubeController : CharacterBody3D
 {
     [Export]
     private float _speed = 7.0f;
     [Export]
-    private float _jumpStrength = 20f;
+    private float _jumpStrength = 25f;
     [Export]
-    private float _fallAcceleration = 75f;
+    private float _fallAcceleration = 50f;
     [Export]
-    private Node3D _camera;
+    public Node3D _camera;
     [Export]
     private InputHandler _input;
 
@@ -39,11 +39,12 @@ public partial class CubeController : CharacterBody3D
         // get jump velocity
         if (IsOnFloor() && _input.ConfirmPressed)
         {
-            _targetVelocity.Y += _jumpStrength;
+            _targetVelocity.Y = _jumpStrength;
         }
 
         // apply target velocity
         Velocity = _targetVelocity;
         MoveAndSlide();
     }
+
 }
